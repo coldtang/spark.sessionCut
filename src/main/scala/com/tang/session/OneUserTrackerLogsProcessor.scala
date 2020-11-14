3a8896d1a2df8bd97d1740b04a652e67e9bd5070
coder <18260105157@163.com>
2020-11-15 01:06:21 +0800

    输出结果数据

--- src/main/scala/com/tang/session/OneUserTrackerLogsProcessor.scala
+++ src/main/scala/com/tang/session/OneUserTrackerLogsProcessor.scala
@@ -1,5 +1,8 @@
 package com.tang.session
 
+import java.net.URL
+import java.util.UUID
+
 import org.apache.commons.lang3.time.FastDateFormat
 
 import scala.collection.mutable.ArrayBuffer
@@ -19,24 +22,63 @@ class OneUserTrackerLogsProcessor(trackerLog: Array[TrackerLog]) {
     *
     * @return
     */
-  def buildSession(): Array[TrackerSession] = {
+  def buildSession(domainLabelMap: Map[String, String]): ArrayBuffer[TrackerSession] = {
     //会话切割
     var cutSessionLogs = new ArrayBuffer[TrackerLog]()
     var initBuilder = ArrayBuffer.newBuilder[ArrayBuffer[TrackerLog]]
-    sortedTrackerLogs.foldLeft((initBuilder, Option.empty[TrackerLog])) {
-      case ((builder, preLog), currLog) =>
-        val currLogTime = dataFormat.parse(currLog.getLogServerTime.toString).getTime
-        val preLogTime = dataFormat.parse(preLog.get.getLogServerTime.toString).getTime
-        if (preLog.nonEmpty && currLogTime - preLogTime >= 30 * 60 * 1000) {
-          builder += cutSessionLogs.clone()
-          cutSessionLogs.clear()
-        }
-        cutSessionLogs += currLog
-        (builder, Some(currLog))
+    val cuttedSessionLogsResult: ArrayBuffer[ArrayBuffer[TrackerLog]] =
+      sortedTrackerLogs.foldLeft((initBuilder, Option.empty[TrackerLog])) {
+        case ((builder, preLog), currLog) =>
+          val currLogTime = dataFormat.parse(currLog.getLogServerTime.toString).getTime
+          if (preLog.nonEmpty) {
+            val preLogTime = dataFormat.parse(preLog.get.getLogServerTime.toString).getTime
+            if (currLogTime - preLogTime > 30 * 60 * 1000) {
+              builder += cutSessionLogs.clone()
+              cutSessionLogs.clear()
+            }
+          }
+          cutSessionLogs += currLog
+          (builder, Some(currLog))
+      }._1.result()
+
+    // 获取最后的会话
+    if (cutSessionLogs.nonEmpty) {
+      cuttedSessionLogsResult += cutSessionLogs
     }
 
     //生成会话
+    cuttedSessionLogsResult.map { case sessionLogs =>
+      val session = new TrackerSession()
+      session.setSessionId(UUID.randomUUID().toString)
+      session.setSessionServerTime(sessionLogs.head.getLogServerTime)
+      session.setCookie(sessionLogs.head.getCookie)
 
-    Array()
+      session.setIp(sessionLogs.head.getIp)
+      val pageviewLogs = sessionLogs.filter(_.getLogType.toString.equals("pageview"))
+      if (pageviewLogs.length == 0) {
+        session.setLandingUrl("-")
+      } else {
+        session.setLandingUrl(pageviewLogs.head.getUrl)
+      }
+      session.setPageviewCount(pageviewLogs.length)
+
+      val clickLogs = sessionLogs.filter(_.getLogType.toString.equals("click"))
+      session.setClickCount(clickLogs.length)
+
+      if (pageviewLogs.length == 0) {
+        session.setDomain("-")
+      } else {
+        val url = new URL(pageviewLogs.head.getUrl.toString)
+        session.setDomain(url.getHost)
+      }
+
+      val domainLabel = domainLabelMap.getOrElse(session.getDomain.toString, "-")
+      session.setDomainLabel(domainLabel)
+
+      // 统计会话的个数
+      //sessionCountAcc.add(1)
+
+      session
+    }
   }
 }
--- src/main/scala/com/tang/session/SessionCutETL.scala
+++ src/main/scala/com/tang/session/SessionCutETL.scala
@@ -1,6 +1,8 @@
 package com.tang.session
 
+import org.apache.parquet.avro.{AvroParquetOutputFormat, AvroWriteSupport}
 import org.apache.spark.rdd.RDD
+import org.apache.spark.storage.StorageLevel
 import org.apache.spark.{SparkConf, SparkContext}
 
 object SessionCutETL {
@@ -16,12 +18,21 @@ object SessionCutETL {
 
     val sc = new SparkContext(conf)
 
+    //网站域名标签map，可以放在数据库中，然后从数据库中捞取出来
+    val domainLabelMap = Map("www.baidu.com" -> "level1", "www.ali.com" -> "level2",
+      "jd.com" -> "level3", "youku.com" -> "level4")
+
+    val domainLabelList = sc.broadcast(domainLabelMap)
+
     // 加载数据
     val rawRDD: RDD[String] = sc.textFile("data/rawdata/visit_log.txt")
 
     // 解析数据并过滤
     val parseLogRDD: RDD[TrackerLog] = rawRDD.flatMap(line => RawLogParser.parse(line))
       .filter(log => logTypeSet.contains(log.getLogType.toString))
+
+    //缓存
+    parseLogRDD.persist(StorageLevel.MEMORY_AND_DISK)
 
     // 分组数据
     val userGroupRDD: RDD[(String, Iterable[TrackerLog])] = parseLogRDD
@@ -30,13 +41,39 @@ object SessionCutETL {
     // 会话切割
     val userSessionRDD: RDD[(String, TrackerSession)] = userGroupRDD.flatMapValues { case iter =>
       val userProcessor = new OneUserTrackerLogsProcessor(iter.toArray)
-      userProcessor.buildSession()
+      userProcessor.buildSession(domainLabelList.value)
     }
 
+    //  给会话的cookie打上标签
+    val cookieLabelRDD: RDD[(String, String)] = sc.textFile("data/cookie_label.txt").map { case line =>
+      val temp = line.split("\\|")
+      (temp(0), temp(1)) // (cookie, cookie_label)
+    }
 
+    val joinRDD: RDD[(String, (TrackerSession, Option[String]))] =
+      userSessionRDD.leftOuterJoin(cookieLabelRDD)
 
+    val cookieLabelSessionRDD: RDD[TrackerSession] = joinRDD.map { case (cookie, (session, cookieLabelOption)) =>
+      if (cookieLabelOption.nonEmpty) {
+        session.setCookieLabel(cookieLabelOption.get)
+      }
+      else {
+        session.setCookieLabel("-")
+      }
+      session
+    }
 
-    userSessionRDD.foreach(println)
+    val baseOutputPath = "data/output/trackerLog"
+    AvroWriteSupport.setSchema(sc.hadoopConfiguration, TrackerLog.SCHEMA$)
+    parseLogRDD.map((null, _)).saveAsNewAPIHadoopFile(baseOutputPath, classOf[Void], classOf[TrackerLog],
+      classOf[AvroParquetOutputFormat[TrackerLog]])
+
+    val baseTrackerSessionOutputPath = "data/output/trackerSession"
+    AvroWriteSupport.setSchema(sc.hadoopConfiguration, TrackerSession.SCHEMA$)
+    cookieLabelSessionRDD.map((null, _)).saveAsNewAPIHadoopFile(baseTrackerSessionOutputPath,
+      classOf[Void], classOf[TrackerSession],
+      classOf[AvroParquetOutputFormat[TrackerSession]])
+    //cookieLabelSessionRDD.collect().foreach(println)
 
     sc.stop()
   }
